8288a2ab4e1c8435629ecda3bcf408d44b55fd12
lizhiq-11 <1114501341@qq.com>
2024-12-17 21:32:18 +0800

    用户敏感信息（手机号、邮箱号）未脱敏查询展示

--- admin/src/main/java/com/lzq/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/lzq/shortlink/admin/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.lzq.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.lzq.shortlink.admin.common.convention.result.Result;
 import com.lzq.shortlink.admin.common.convention.result.Results;
 import com.lzq.shortlink.admin.common.enums.UserErrorCodeEnum;
+import com.lzq.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.lzq.shortlink.admin.dto.resp.UserRespDTO;
 import com.lzq.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,7 +26,7 @@ public class UserController {
     /**
      *根据用户名查询用户信息
      */
-    @GetMapping("/api/shortlink/v1/user/{username}")
+    @GetMapping("/api/short-link/admin/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
         UserRespDTO result = userService.getUserByUsername(username);
         if(result == null){
@@ -31,5 +34,21 @@ public class UserController {
         }else{
             return Results.success(result);
         }
+    }
+
+    /**
+     * 根据用户名查询无脱敏用户信息
+     */
+    @GetMapping("/api/short-link/admin/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username) {
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
+    }
+
+    /**
+     * 查询用户名是否存在
+     */
+    @GetMapping("/api/short-link/admin/v1/user/has-username")
+    public Result<Boolean> hasUsername(@RequestParam("username") String username) {
+        return Results.success(userService.hasUsername(username));
     }
 }
