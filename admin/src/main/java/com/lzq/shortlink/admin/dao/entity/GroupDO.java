9c11f8ac76da79f4f8a7d1dc8b68aa60f67482c0
lizhiq-11 <1114501341@qq.com>
2024-12-19 15:10:04 +0800

    新增短链接创建分组功能

--- admin/src/main/java/com/lzq/shortlink/admin/dao/entity/GroupDO.java
+++ admin/src/main/java/com/lzq/shortlink/admin/dao/entity/GroupDO.java
@@ -1,16 +1,21 @@
 package com.lzq.shortlink.admin.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.lzq.shortlink.admin.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-
-import java.util.Date;
+import lombok.NoArgsConstructor;
 
 /**
  * 短链接分组实体
  */
 @Data
 @TableName("t_group")
-public class GroupDO {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupDO extends BaseDO {
     /**
      * id
      */
@@ -35,20 +40,4 @@ public class GroupDO {
      * 分组排序
      */
     private Integer sortOrder;
-
-    /**
-     * 创建时间
-     */
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    private Integer delFlag;
-
 }
--- admin/src/main/java/com/lzq/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/lzq/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,16 +1,67 @@
 package com.lzq.shortlink.admin.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lzq.shortlink.admin.common.biz.user.UserContext;
 import com.lzq.shortlink.admin.dao.entity.GroupDO;
 import com.lzq.shortlink.admin.dao.mapper.GroupMapper;
+import com.lzq.shortlink.admin.dao.mapper.GroupUniqueMapper;
 import com.lzq.shortlink.admin.service.GroupService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
-/**
- * 短链接分组接口实现层
- */
+import java.util.List;
+
+import static com.lzq.shortlink.admin.common.constant.RedisCacheConstant.LOCK_GROUP_CREATE_KEY;
+
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    private final RBloomFilter<String> gidRegisterCachePenetrationBloomFilter;
+    private final GroupUniqueMapper groupUniqueMapper;
+    private final RedissonClient redissonClient;
+
+    @Override
+    public void saveGroup(String groupName) {
+        saveGroup(UserContext.getUsername(), groupName);
+    }
+
+    @Override
+    public void saveGroup(String username, String groupName) {
+        RLock lock = redissonClient.getLock(String.format(LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, username)
+                    .eq(GroupDO::getDelFlag, 0);
+            List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+            int retryCount = 0;
+            int maxRetries = 10;
+            String gid = null;
+            while (retryCount < maxRetries) {
+                if (StrUtil.isNotEmpty(gid)) {
+                    GroupDO groupDO = GroupDO.builder()
+                            .gid(gid)
+                            .sortOrder(0)
+                            .username(username)
+                            .name(groupName)
+                            .build();
+                    baseMapper.insert(groupDO);
+                    gidRegisterCachePenetrationBloomFilter.add(gid);
+                    break;
+                }
+                retryCount++;
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
 }
