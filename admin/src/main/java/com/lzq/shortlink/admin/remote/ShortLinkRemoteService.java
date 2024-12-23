349aa5b0eb9a63cf6d3e2e07bffc06baa1895f86
lizhiq-11 <1114501341@qq.com>
2024-12-23 11:52:25 +0800

    feature：短链接后台管理-开发短链接修改功能

--- admin/src/main/java/com/lzq/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/lzq/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -7,9 +7,10 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.lzq.shortlink.admin.common.convention.result.Result;
 import com.lzq.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.lzq.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.lzq.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.lzq.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
-import com.lzq.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 import com.lzq.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
+import com.lzq.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 
 import java.util.HashMap;
 import java.util.List;
@@ -30,6 +31,15 @@ public interface ShortLinkRemoteService {
         String resultBodyStr = HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/create", JSON.toJSONString(requestParam));
         return JSON.parseObject(resultBodyStr, new TypeReference<>() {
         });
+    }
+
+    /**
+     * 修改短链接
+     *
+     * @param requestParam 修改短链接请求参数
+     */
+    default void updateShortLink(ShortLinkUpdateReqDTO requestParam){
+        HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/update", JSON.toJSONString(requestParam));
     }
 
     /**
